ff773241c09a07bc752c73941d5cc5cdadec2c47
Nina Loeterman <nina.loeterman@gmail.com>
2021-06-25 13:34:16 +0300

    basic message sending set up with mock account

--- App.js
+++ App.js
@@ -1,14 +1,49 @@
 const express = require("express");
 const cors = require("cors");
+const nodemailer = require("nodemailer");
 const app = express();
 
 const port = 5050;
-app.use(cors());
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.listen(port, () => {
-    console.log(`portfolio_server listening at http://localhost:${port}`);
-})
+  console.log(`portfolio_server listening at http://localhost:${port}`);
+});
+
+app.post("/messages", (req, res) => {
+  const messageData = req.body.data;
+  console.log("message data:", messageData);
+  res.send(messageData);
+  sendMessage(messageData).catch(
+    console.error("an error occured while sending your message", e)
+  );
+});
+
+const sendMessage = async (messageData) => {
+  const transporter = nodemailer.createTransport({
+    service: "outlook",
+    port: 25,
+    auth: {
+      user: "spaceholder@outlook.com", //fill this in,
+      pass: "spaceholder", //fill this in
+    },
+  });
+
+  const message = {
+    from: "placeholder@outlook.com",
+    to: "nina.loeterman@gmail.com",
+    subject: "Someone wants to be in touch!",
+    html: ` <div>Name: ${messageData.name}</div>
+            <div>Email: ${messageData.email}</div>
+            <div>Message: ${messageData.message}</div>`,
+  };
+
+  transporter.sendMail(message, (error, info) => {
+    error
+      ? console.log("an error occured", error)
+      : console.log(`message sent: ${info.response}`);
+  });
+};
